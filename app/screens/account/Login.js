91c249bc111b2dd4215403546f79137b879f3bd0
luvantrong <102342647+luvantrong@users.noreply.github.com>
2023-06-01 12:15:20 +0700

    Trong LV
    API Login and Register

--- app/screens/account/Login.js
+++ app/screens/account/Login.js
@@ -7,37 +7,89 @@ import {
   Pressable,
   Image,
   Alert,
-} from 'react-native';
-import {AccountContext} from '../../context/AccountContext';
-import React, {useContext} from 'react';
+  ToastAndroid,
+} from "react-native";
+import { AccountContext } from "../../context/AccountContext";
+import React, { useContext, useState, useEffect } from "react";
+import config from "../../config/config";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Login = props => {
-  const {navigation} = props;
-  const {setIsLoggedIn} = useContext(AccountContext);
+const Login = (props) => {
+  const { navigation } = props;
+  const { setIsLoggedIn, setDataAccount, rememberEmailRegister } =
+    useContext(AccountContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    setEmail(rememberEmailRegister);
+    return () => {};
+  }, [rememberEmailRegister]);
 
   const handleLogin = async () => {
-    setIsLoggedIn(true);
+    let data = { email, password };
+    const fetchData = async (data) => {
+      let url = `${config.CONSTANTS.IP}api/account/login`;
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          // Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const res = await response.json();
+      return res;
+    };
+    const res = await fetchData(data);
+    console.log(res);
+
+    if (res.result) {
+      setIsLoggedIn(true);
+      setDataAccount(res.account);
+      await AsyncStorage.setItem("token", res.token);
+    } else {
+      ToastAndroid.show(res.message, ToastAndroid.SHORT);
+    }
+  };
+
+  const handleRegister = () => {
+    navigation.navigate("Register");
   };
   return (
     <ScrollView style={styles.Container}>
-      <Text
-        style={{
-          textAlign: 'right',
-          color: 'black',
-          fontFamily: 'Popins',
-          marginRight: 20,
-          marginTop: 20,
-          fontWeight: 'bold',
-        }}>
-        Register
-      </Text>
+      <Pressable onPress={handleRegister}>
+        <Text
+          style={{
+            textAlign: "right",
+            color: "black",
+            fontFamily: "Popins",
+            marginRight: 20,
+            marginTop: 20,
+            fontWeight: "bold",
+          }}
+        >
+          Register
+        </Text>
+      </Pressable>
+
       <View style={styles.ViewTextSignIn}>
         <Text style={styles.TextSignIn}>Sign In</Text>
       </View>
       <View style={styles.ViewTextInput}>
-        <TextInput style={styles.TextInput} placeholder="Username"></TextInput>
-        <TextInput style={styles.TextInput} placeholder="Password"></TextInput>
-        <Text style={{textAlign: 'right', color: 'black'}}>
+        <TextInput
+          value={email}
+          onChangeText={setEmail}
+          style={styles.TextInput}
+          placeholder="Username"
+        ></TextInput>
+        <TextInput
+          value={password}
+          onChangeText={setPassword}
+          style={styles.TextInput}
+          placeholder="Password"
+        ></TextInput>
+        <Text style={{ textAlign: "right", color: "black" }}>
           Forgot Password?
         </Text>
         <Pressable style={styles.Button} onPress={handleLogin}>
@@ -45,29 +97,33 @@ const Login = props => {
         </Pressable>
         <Pressable style={styles.ButtonGoogle}>
           <Image
-            source={require('../../images/ggicon.png')}
-            style={{width: 30, height: 30}}></Image>
+            source={require("../../images/ggicon.png")}
+            style={{ width: 30, height: 30 }}
+          ></Image>
           <Text
             style={{
               margin: 20,
-              fontWeight: 'bold',
+              fontWeight: "bold",
               fontSize: 17,
-              color: 'black',
-            }}>
+              color: "black",
+            }}
+          >
             Continue with Google
           </Text>
         </Pressable>
         <Pressable style={styles.ButtonGoogle}>
           <Image
-            source={require('../../images/fbicon.png')}
-            style={{width: 30, height: 30}}></Image>
+            source={require("../../images/fbicon.png")}
+            style={{ width: 30, height: 30 }}
+          ></Image>
           <Text
             style={{
               margin: 20,
-              fontWeight: 'bold',
+              fontWeight: "bold",
               fontSize: 17,
-              color: 'black',
-            }}>
+              color: "black",
+            }}
+          >
             Continue with Facebook
           </Text>
         </Pressable>
@@ -75,13 +131,14 @@ const Login = props => {
     </ScrollView>
   );
 };
+
 export default Login;
 
 const styles = StyleSheet.create({
   Container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'pink',
+    flexDirection: "column",
+    backgroundColor: "pink",
   },
   ViewTextSignIn: {
     paddingTop: 50,
@@ -89,13 +146,13 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
   },
   TextSignIn: {
-    fontFamily: 'Popins',
-    color: 'black',
-    fontWeight: '700',
+    fontFamily: "Popins",
+    color: "black",
+    fontWeight: "700",
     fontSize: 30,
   },
   ViewTextInput: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderTopLeftRadius: 50,
     borderTopRightRadius: 50,
     paddingLeft: 20,
@@ -107,13 +164,13 @@ const styles = StyleSheet.create({
   TextInput: {
     height: 60,
     borderRadius: 20,
-    backgroundColor: '#F5F5F5',
+    backgroundColor: "#F5F5F5",
     marginTop: 10,
     marginBottom: 10,
-    fontFamily: 'Popins',
+    fontFamily: "Popins",
     fontSize: 15,
     paddingLeft: 30,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   Button: {
     height: 60,
@@ -121,15 +178,15 @@ const styles = StyleSheet.create({
     width: 350,
     marginTop: 16,
     marginBottom: 16,
-    alignItems: 'center',
-    backgroundColor: 'black',
-    justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "black",
+    justifyContent: "center",
   },
   TextButton: {
     fontSize: 20,
-    color: 'white',
-    fontFamily: 'Popins',
-    fontWeight: 'bold',
+    color: "white",
+    fontFamily: "Popins",
+    fontWeight: "bold",
   },
   ButtonGoogle: {
     height: 60,
@@ -137,13 +194,13 @@ const styles = StyleSheet.create({
     width: 350,
     marginTop: 16,
     marginBottom: 16,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    backgroundColor: '#F5F5F5',
-    shadowColor: 'rgba(0, 0, 0, 0.1)',
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    backgroundColor: "#F5F5F5",
+    shadowColor: "rgba(0, 0, 0, 0.1)",
     shadowOpacity: 0.8,
-    shadowOffset: {width: 1, height: 13},
+    shadowOffset: { width: 1, height: 13 },
     paddingLeft: 30,
   },
 });
--- app/screens/account/Register.js
+++ app/screens/account/Register.js
@@ -5,29 +5,87 @@ import {
   StyleSheet,
   TextInput,
   Pressable,
-} from 'react-native';
+  ToastAndroid,
+} from "react-native";
+import React, { useContext, useState } from "react";
+import config from "../../config/config";
+import { AccountContext } from "../../context/AccountContext";
 
-export default function Register() {
+const Register = (props) => {
+  const { navigation } = props;
+  const { setRememberEmailRegister } = useContext(AccountContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [name, setName] = useState("");
+
+  const handleRegister = async () => {
+    let data = { email, password, name };
+    const fetchData = async (data) => {
+      let url = `${config.CONSTANTS.IP}api/account/register`;
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      const res = await response.json();
+      return res;
+    };
+    const res = await fetchData(data);
+    console.log(res);
+    if (res.result) {
+      ToastAndroid.show("Đăng ký thành công", ToastAndroid.SHORT);
+      setRememberEmailRegister(email);
+      navigation.navigate("Login");
+    } else {
+      const mes = res.message;
+      ToastAndroid.show(mes, ToastAndroid.SHORT);
+    }
+  };
   return (
     <ScrollView style={styles.Container}>
       <View style={styles.ViewTextSignIn}>
         <Text style={styles.TextSignIn}>Sign Up</Text>
       </View>
       <View style={styles.ViewTextInput}>
-        <TextInput style={styles.TextInput} placeholder="Username"></TextInput>
-        <TextInput style={styles.TextInput} placeholder="Password"></TextInput>
-        <Pressable style={styles.Button}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        ></TextInput>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+        ></TextInput>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Verifi Password"
+          value={rePassword}
+          onChangeText={setRePassword}
+        ></TextInput>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Name"
+          value={name}
+          onChangeText={setName}
+        ></TextInput>
+        <Pressable style={styles.Button} onPress={handleRegister}>
           <Text style={styles.TextButton}>Sign Up</Text>
         </Pressable>
       </View>
     </ScrollView>
   );
-}
+};
+
+export default Register;
 const styles = StyleSheet.create({
   Container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'pink',
+    flexDirection: "column",
+    backgroundColor: "pink",
   },
   ViewTextSignIn: {
     paddingTop: 50,
@@ -35,13 +93,13 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
   },
   TextSignIn: {
-    fontFamily: 'Popins',
-    color: 'black',
-    fontWeight: '700',
+    fontFamily: "Popins",
+    color: "black",
+    fontWeight: "700",
     fontSize: 30,
   },
   ViewTextInput: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderTopLeftRadius: 50,
     borderTopRightRadius: 50,
     paddingLeft: 20,
@@ -53,27 +111,27 @@ const styles = StyleSheet.create({
   TextInput: {
     height: 60,
     borderRadius: 20,
-    backgroundColor: '#F5F5F5',
+    backgroundColor: "#F5F5F5",
     marginTop: 10,
     marginBottom: 10,
-    fontFamily: 'Popins',
+    fontFamily: "Popins",
     fontSize: 15,
     paddingLeft: 30,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   Button: {
     height: 60,
     borderRadius: 30,
     marginTop: 16,
     marginBottom: 16,
-    alignItems: 'center',
-    backgroundColor: 'black',
-    justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "black",
+    justifyContent: "center",
   },
   TextButton: {
     fontSize: 20,
-    color: 'white',
-    fontFamily: 'Popins',
-    fontWeight: 'bold',
+    color: "white",
+    fontFamily: "Popins",
+    fontWeight: "bold",
   },
 });
